7bdad25e00641d4a277773e30c4912f0f68a3257
veronika_t <splot2023@mail.ru>
2025-03-22 08:13:18 +0100

    Deleted all comments, so to clean the file

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    //new comment
-    // one more comment
-    //comment 123
-    // new Veronika comment
-
-    // one more comment from Anna
-
-    // hello world11
-
-    //c hello from Veronika
-    // last comment
-    // 3456
-    //comment from Anna 20.03.
-    // change in IDEA from Anna
-    // change in GitHub from Anna
-    // 2 change in IDEA from Anna
-    //New Feature started. LoginButton test. New feature goes on. Some new corrections
-    //ABC feature Go on test is ready
-    //XYZ feature started go on  test is ready
-
-
-
 
 }
